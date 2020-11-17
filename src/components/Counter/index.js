c8f584899c02b02e3de1a5753edc4b54f3c1af3b
Hazieon <handrew3721@gmail.com>
2020-11-17 10:41:22 +0000

    usereducer

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,23 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from "react";
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return { count: state.count + 1 };
+    case "subtract":
+      return { count: state.count - 1 };
+
+    default:
+      throw new Error();
+  }
+}
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  function incrementCounter() {
-    const updatedCounter = counter + 1;
-    setCounter(updatedCounter);
-  }
+  // function incrementCounter() {
+  //   const updatedCounter = counter + 1;
+  //   setCounter(updatedCounter);
+  // }
 
-  function decrementCounter() {
-    const updatedCounter = counter - 1;
-    setCounter(updatedCounter);
-  }
+  // function decrementCounter() {
+  //   const updatedCounter = counter - 1;
+  //   setCounter(updatedCounter);
+  // }
 
   return (
     <div>
-      <button onClick={incrementCounter}>+</button>
-      <p>{counter}</p>
-      <button onClick={decrementCounter}>-</button>
+      <button onClick={() => dispatch({ type: "add" })}>+</button>
+      <p>counter:{state.count}</p>
+      <button onClick={() => dispatch({ type: "subtract" })}>-</button>
     </div>
   );
 }
